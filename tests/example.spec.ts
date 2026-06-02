ff2fae0db222ae27f57feff6805720218fc7adf0
bryanchoong52 <bryanchoong52@gmail.com>
2026-06-02 21:47:37 +0800

    comment playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,15 +4,15 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  //await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  //await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
