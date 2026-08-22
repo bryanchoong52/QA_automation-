e32415863cbc70e51e3485a587a527fa72c713c9
bryanchoong52 <bryanchoong52@gmail.com>
2026-08-22 22:48:01 +0800

    Add test case for test the UI test navigattion

--- playwright/tests/testautomation2.spec.ts
+++ playwright/tests/testautomation2.spec.ts
@@ -2,6 +2,38 @@ import {test, expect, type Page } from '@playwright/test';
 import { credentialsForgeek, credentialsInvalidForgeek} from '../Test data/credential';
 
 
+// Test case - open geeksforgeeks website and access the python page and then access the array page
+test('open geeksforgeeks website', async ({ page }) => {
+  await page.goto('https://www.geeksforgeeks.org/');
+
+  // check the title of the page
+  await expect(page).toHaveTitle(/GeeksforGeeks/);
+  await page.waitForTimeout(3000); // wait 3 seconds
+
+  // click on the Python link
+  //await page.getByRole("link", { name: "Python" }).click();
+
+  await page.locator("a").filter({ hasText: "Python" }).first().click();
+  // check the sub title for data structures and algorithm
+
+  await page.waitForTimeout(3000); 
+  //Scrolls to the exact bottom of the page
+  await page.evaluate(() => window.scrollTo(0, 500));
+
+  await page.locator("span").filter({ hasText: "Data Structures" }).first().scrollIntoViewIfNeeded();
+  await page.waitForTimeout(3000); // wait 3 seconds
+
+  // click the link to array page 
+
+  await page.locator("a").filter({ hasText: "Arrays" }).first().click();
+  // check the heading for python arrays after clicked from the python page
+  await page.getByRole("heading", { name: "Python Arrays" }).isVisible();
+
+  // hold for 3 seconds to see the result
+  await page.waitForTimeout(3000); // wait 3 seconds
+});
+
+
 
 // Test case - valid login
 test('Login Valid', async ({ page }) => {
@@ -52,5 +84,7 @@ test('invalid login', async ({ page }) => {
   await page.waitForTimeout(3000); // wait 3 seconds
 
 });
+
+
 
 
